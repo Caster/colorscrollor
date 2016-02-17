e10cda1494a7b460b5762962c1398ca05f97eb20
Thom Castermans <thomcastermans@users.sourceforge.net>
2016-02-17 18:10:48 +0100

    Play/pause works now!

--- scripts/colorscrollor.js
+++ scripts/colorscrollor.js
@@ -19,27 +19,67 @@
             colors = [ // light and dark colors as RGB (colorbrewer Blues 9)
                 [247, 251, 255], // light
                 [  8,  48, 107] // dark
-            ];
+            ],
+            playInterval = null,
+            self = this;
 
         // constructor
         container.style.width = n + 'em';
+        self.playing = false;
 
-        this.addRandom = function() {
-            var i = 0,
-                li = w.document.createElement('li');
-            for (; i < n; ++i) {
+        self.addRandom = function() {
+            var li = w.document.createElement('li');
+            for (let i = 0; i <= n; ++i) {
                 var b = w.document.createElement('span'),
                     r = Math.floor(Math.random() * c);
                 b.classList.add('block');
-                b.style.backgroundColor = rgbToHex(
-                    colors[0][0] + (colors[1][0] - colors[0][0]) / (c - 1) * r,
-                    colors[0][1] + (colors[1][1] - colors[0][1]) / (c - 1) * r,
-                    colors[0][2] + (colors[1][2] - colors[0][2]) / (c - 1) * r
-                );
+                if (i > 0) {
+                    b.style.backgroundColor = rgbToHex(
+                        colors[0][0] + (colors[1][0] - colors[0][0]) / (c - 1) * r,
+                        colors[0][1] + (colors[1][1] - colors[0][1]) / (c - 1) * r,
+                        colors[0][2] + (colors[1][2] - colors[0][2]) / (c - 1) * r
+                    );
+                } else {
+                    // extra dummy block for playing animation
+                    b.style.backgroundColor = 'red';
+                    b.style.marginLeft = '-1em';
+                }
                 li.appendChild(b);
             }
             container.appendChild(li);
         };
+
+        self.play = function() {
+            if (self.playing)  return;
+            self.playing = true;
+            self.shift();
+            playInterval = w.setInterval(function() {
+                if (self.playing) {
+                    self.shift();
+                } else {
+                    w.clearInterval(playInterval);
+                    playInterval = null;
+                }
+            }, 400);
+        }
+
+        self.pause = function() {
+            self.playing = false;
+        }
+
+        self.shift = function() {
+            var lis = container.children, li;
+            for (let i = 0; i < lis.length; ++i) {
+                li = lis.item(i);
+                // copy color of first block to dummy
+                li.children.item(0).style.backgroundColor =
+                    li.children.item(1).style.backgroundColor;
+                li.children.item(0).style.marginLeft = '';
+                li.appendChild(li.children.item(0)); // move dummy to back
+                // do actual shift
+                li.children.item(0).style.marginLeft = '-1em';
+            }
+        }
 
     };
 
--- scripts/demo.js
+++ scripts/demo.js
@@ -1,11 +1,34 @@
+
 (function(window) {
 
     var cs = new window.ColorScrollor(
             window.document.getElementsByClassName('scrollor').item(0)
-        );
+        ),
+        // buttons
+        add = window.document.getElementById('cntrl-add'),
+        rnd = window.document.getElementById('cntrl-rnd'),
+        ply = window.document.getElementById('cntrl-ply');
 
-    window.document.getElementById('cntrl-rnd').addEventListener('click', function() {
+    // for testing TODO remove
+    for (let i = 0; i < 10; ++i) {
         cs.addRandom();
+    }
+
+    // add randomly generated list
+    rnd.addEventListener('click', function() {
+        cs.addRandom();
+    });
+
+    // play/pause the scrolling
+    ply.addEventListener('click', function() {
+        var plyIcon = ply.children.item(0);
+        plyIcon.classList.toggle('fa-play');
+        plyIcon.classList.toggle('fa-pause');
+        if (cs.playing) {
+            cs.pause();
+        } else {
+            cs.play();
+        }
     });
 
 }(window));
